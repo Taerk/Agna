8ecc0e571d138eec799216860f236d333ebe89c5
Taerk <taerkex@gmail.com>
2016-02-06 10:17:25 -0500

    Initial Polarity commentator cam

--- html5/overlays/js/agna.overlay.polarity.cam.js
+++ html5/overlays/js/agna.overlay.polarity.cam.js
@@ -1,11 +1,16 @@
-var rotation = 0;
-var rotate_speed = 5;
-var yoffset = 50;
-
 var compass_piece_a = new Image();
 compass_piece_a.src = 'images/compass-piece-a.png';
 var compass_piece_b = new Image();
 compass_piece_b.src = 'images/compass-piece-b.png';
+
+var com_back = new Image();
+com_back.src = 'polarity-overlay-camera-bottom.png';
+
+var com_left = new Image();
+com_left.src  = 'polarity-overlay-camera-left.png';
+
+var com_right = new Image();
+com_right.src  = 'polarity-overlay-camera-right.png';
 
 function drawOverlay() {
 	agna.ctx.clearRect(0,0,1280,720);
@@ -14,9 +19,7 @@ function drawOverlay() {
 		var border_gradient;
 		border_gradient = agna.ctx.createLinearGradient(0, agna.OVERLAY_HEIGHT - 20, 0, agna.OVERLAY_HEIGHT);
 		border_gradient.addColorStop(0, "transparent");
-		border_gradient.addColorStop(1, "rgba(0,0,0,0.4)");
-		
-		drawPlayers();
+		border_gradient.addColorStop(1, "rgba(0,0,0,0.2)");
 		
 		agna.ctx.fillStyle = border_gradient;
 		agna.ctx.beginPath();
@@ -24,12 +27,23 @@ function drawOverlay() {
 		agna.ctx.closePath();
 		agna.ctx.fill();
 		
+		drawBottom();
+		if (agna.getField(field_left).trim() != '') {
+			drawLeft();
+		}
+		if (agna.getField(field_right).trim() != '') {
+			drawRight();
+		}
 		drawText();
-		drawCompass();
 	}
 }
 
 function drawText() {
+	var yoffset = 0;
+	
+	/**
+	 * CLOCK
+	 */
 	d = new Date();
 	time_text = (d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear().toString().substr(2,2) + " - " + agna.twelveHour(d.getHours()) + ":" + agna.pad(d.getMinutes(), 2, 0);
 	if (d.getHours() >= 12) {
@@ -40,7 +54,7 @@ function drawText() {
 	
 	agna.ctx.fillStyle = '#444';
 	agna.ctx.strokeStyle = 'black';
-	agna.ctx.font = "bold small-caps 1.4em Zekton";
+	agna.ctx.font = "bold small-caps 1.2em Zekton";
 	agna.ctx.lineWidth = 3;
 	
 	agna.ctx.textAlign = 'left';
@@ -55,68 +69,20 @@ function drawText() {
 	// agna.ctx.strokeText(camera_name, agna.ctx.width / 2, agna.ctx.height - 10);
 	// agna.ctx.fillText(camera_name, agna.ctx.width / 2, agna.ctx.height - 10);
 	
-	agna.ctx.fillStyle = '#eee';
-	agna.ctx.font = "2.4em Zekton";
-	agna.ctx.fillText(agna.cleanText(agna.getField(field_left), field_left), 355, 550 + yoffset, 440);
-	agna.ctx.fillText(agna.cleanText(agna.getField(field_right), field_right), 925, 550 + yoffset, 440);
+	agna.ctx.font = "1.6em Zekton";
+	agna.ctx.fillStyle = '#333';
+	agna.ctx.fillText(agna.cleanText(agna.getField(field_left), field_left), 285, 635 + yoffset, 300);
+	agna.ctx.fillText(agna.cleanText(agna.getField(field_right), field_right), 995, 635 + yoffset, 300);
 }
 
-function drawCompass() {
-	agna.ctx.drawImage(compass_piece_a, 590, 500 + yoffset, 100, 100);
-	
-	/* if (rotation < 360) {
-		rotation += rotate_speed;
-		rotate_speed += 5;
-	} */
-	// agna.ctx.save(); 
-	// agna.ctx.translate(640, 550);
-	// agna.ctx.rotate(rotation * Math.PI / 180);
-	// agna.ctx.drawImage(compass_piece_b, -50, -50, 100, 100);
-	// agna.ctx.restore();
-	agna.ctx.drawImage(compass_piece_b, 590, 500 + yoffset, 100, 100);
+function drawBottom() {
+	agna.ctx.drawImage(com_back, 0, 0);
 }
 
-function drawPlayers() {
-	
-	// Gray
-	agna.ctx.fillStyle = '#666';
-	agna.ctx.beginPath();
-	agna.ctx.moveTo(100, 510 + yoffset);
-	agna.ctx.lineTo(640, 510 + yoffset);
-	agna.ctx.lineTo(640, 580 + yoffset);
-	agna.ctx.lineTo(120, 580 + yoffset);
-	agna.ctx.closePath();
-	agna.ctx.fill();
-	
-	// Red
-	agna.ctx.fillStyle = '#a66';
-	agna.ctx.beginPath();
-	agna.ctx.moveTo(640, 510 + yoffset);
-	agna.ctx.lineTo(1180, 510 + yoffset);
-	agna.ctx.lineTo(1160, 580 + yoffset);
-	agna.ctx.lineTo(640, 580 + yoffset);
-	agna.ctx.closePath();
-	agna.ctx.fill();
-	
-	agna.ctx.globalAlpha = 0.4;
-	agna.ctx.fillStyle = 'black';
-	// agna.ctx.globalCompositeOperation = 's';
-	agna.ctx.beginPath();
-	agna.ctx.rect(0, 560 + yoffset, 1280, 720);
-	agna.ctx.closePath();
-	agna.ctx.fill();
-	agna.ctx.globalAlpha = 1;
-	
-	agna.ctx.globalCompositeOperation = 'destination-in';
-	
-	agna.ctx.beginPath();
-	agna.ctx.moveTo(100, 510 + yoffset);
-	agna.ctx.lineTo(1180, 510 + yoffset);
-	agna.ctx.lineTo(1160, 580 + yoffset);
-	agna.ctx.lineTo(120, 580 + yoffset);
-	agna.ctx.closePath();
-	agna.ctx.fill();
-	
-	agna.ctx.globalCompositeOperation = 'source-over';
-	
+function drawLeft() {
+	agna.ctx.drawImage(com_left, 0, 0);
+}
+
+function drawRight() {
+	agna.ctx.drawImage(com_right, 0, 0);
 }
